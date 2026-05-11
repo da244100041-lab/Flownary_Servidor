fda9571ec8430d46ce4c0bb7f9e8a28fd080d3de
Victor Dubon <vmda.development@gmail.com>
2026-05-11 08:40:09 -0600

    Initial commit

---
+++ src/main/java/com/codepro/flownary_servidor/FlownaryServidorApplication.java
@@ -0,0 +1,13 @@
+package com.codepro.flownary_servidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlownaryServidorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlownaryServidorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codepro/flownary_servidor/FlownaryServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codepro.flownary_servidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlownaryServidorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
